365e2c7d8e1f0d6faf611dd8e422f89c603d53d1
Caio Rossini <caio.rossini.dev@gmail.com>
2026-05-26 18:02:34 -0300

    update: admin.py updated for list display and filter

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task, Tag
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "is_done", "created_at", "deadline"]
+    list_filter = ["is_done", "tags"]
+
+admin.site.register(Tag)
